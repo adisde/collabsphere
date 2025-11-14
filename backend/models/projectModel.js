355e5483ebcbfff0d0da7651582e4af566b14013
adisde <adityaa.sde@gmail.com>
2025-11-14 17:40:59 +0530

    update project model

--- backend/models/projectModel.js
+++ backend/models/projectModel.js
@@ -1,37 +1,48 @@
 import pool from "../config/db.js";
 
 export default class Project {
-    // Create Project
+  // Create Project
 
-    static async createProject({ title, description, owner_id }) {
-        const query = `INSERT INTO projects (title, description, owner_id) VALUES ($1, $2, $3) RETURNING *;`;
-        const { rows } = await pool.query(query, [title, description, owner_id]);
-        return rows[0];
-    }
+  static async createProject({ title, description, owner_id }) {
+    const query = `INSERT INTO projects (title, description, owner_id) VALUES ($1, $2, $3) RETURNING *;`;
+    const { rows } = await pool.query(query, [title, description, owner_id]);
+    return rows[0];
+  }
 
-    // Update Project
+  // Update Project
 
-    static async updateProject({ title, description, owner_id, id }) {
-        const query = `UPDATE projects SET title = $1, description = $2, owner_id = $3 WHERE id = $4 RETURNING *;`;
-        const { rows } = await pool.query(query, [title, description, owner_id, id]);
-        return rows[0];
-    }
+  static async updateProject({ title, description, owner_id, id }) {
+    const query = `UPDATE projects SET title = $1, description = $2, owner_id = $3 WHERE id = $4 RETURNING *;`;
+    const { rows } = await pool.query(query, [
+      title,
+      description,
+      owner_id,
+      id,
+    ]);
+    return rows[0];
+  }
 
-    // Delete Project 
+  // Delete Project
 
-    static async removeProject({ id }) {
-        const query = `DELETE FROM projects WHERE id = $1 RETURNING *;`;
-        const { rows } = await pool.query(query, [id]);
-        return rows[0];
-    }
+  static async removeProject({ id }) {
+    const query = `DELETE FROM projects WHERE id = $1 RETURNING *;`;
+    const { rows } = await pool.query(query, [id]);
+    return rows[0];
+  }
 
-    // Search Project For Owner
+  // Search Project For Owner
 
-    static async searchProject({ user_id }) {
-        const query = `SELECT * FROM projects WHERE owner_id = $1 RETURNING *;`;
-        const { rows } = await pool.query(query, [user_id]);
-        return rows;
-    }
+  static async searchProject({ user_id }) {
+    const query = `SELECT * FROM projects WHERE owner_id = $1;`;
+    const { rows } = await pool.query(query, [user_id]);
+    return rows[0];
+  }
 
-    // 
+  // Search Project with Project Id
+
+  static async searchProjectId({ project_id }) {
+    const query = `SELECT * FROM projects WHERE id = $1;`;
+    const { rows } = await pool.query(query, [project_id]);
+    return rows[0];
+  }
 }
