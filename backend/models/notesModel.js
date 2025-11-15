9a40a78a72ed9b2206f8c821fc72c866489b780f
adisde <adityaa.sde@gmail.com>
2025-11-15 17:28:02 +0530

    update notes model

--- backend/models/notesModel.js
+++ backend/models/notesModel.js
@@ -1,35 +1,54 @@
 import pool from "../config/db.js";
 
 export default class Notes {
-    // Create Note
+  // Create Note
 
-    static async createNote({ project_id, user_id, title, content }) {
-        const query = "INSERT INTO notes (project_id, user_id, title, content) VALUES ($1, $2, $3, $4) RETURNING *;";
-        const { rows } = await pool.query(query, [project_id, user_id, title, content]);
-        return rows[0];
-    }
+  static async createNote({ project_id, user_id, title, content }) {
+    const query =
+      "INSERT INTO notes (project_id, user_id, title, content) VALUES ($1, $2, $3, $4) RETURNING *;";
+    const { rows } = await pool.query(query, [
+      project_id,
+      user_id,
+      title,
+      content,
+    ]);
+    return rows[0];
+  }
 
-    // Get Notes 
+  // Get Notes
 
-    static async getNotes({ project_id }) {
-        const query = "SELECT * FROM notes WHERE project_id = $1;";
-        const { rows } = await pool.query(query, [project_id]);
-        return rows;
-    }
+  static async getNotes({ project_id }) {
+    const query = "SELECT * FROM notes WHERE project_id = $1;";
+    const { rows } = await pool.query(query, [project_id]);
+    return rows;
+  }
 
-    // Update Note
+  // Get Note (Single)
 
-    static async updateNote({ project_id, title, content }) {
-        const query = "UPDATE notes SET title = $2, content = $3 WHERE project_id = $1 RETURNING *;";
-        const { rows } = await pool.query(query, [project_id, title, content]);
-        return rows;
-    }
+  static async getNote({ id }) {
+    const query = "SELECT * FROM notes WHERE id = $1;";
+    const { rows } = await pool.query(query, [id]);
+    return rows[0];
+  }
 
-    // Delete Note 
+  // Update Note
 
-    static async removeNote({ id }) {
-        const query = "DELETE FROM notes WHERE id = $1 RETURNING *;";
-        const { rows } = await pool.query(query, [id]);
-        return rows;
-    }
+  static async updateNote({ title, content, id }) {
+    const query =
+      "UPDATE notes SET title = $1, content = $2 WHERE id = $3 RETURNING *;";
+    const { rows } = await pool.query(query, [
+      title,
+      content,
+      id,
+    ]);
+    return rows[0];
+  }
+
+  // Delete Note
+
+  static async removeNote({ id }) {
+    const query = "DELETE FROM notes WHERE id = $1 RETURNING *;";
+    const { rows } = await pool.query(query, [id]);
+    return rows[0];
+  }
 }
