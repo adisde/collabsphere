93752231709d4abeac6e703ff0a2da83a2c97954
adisde <adityaa.sde@gmail.com>
2025-11-19 23:53:34 +0530

    update project model (optimized)

--- backend/models/projectModel.js
+++ backend/models/projectModel.js
@@ -1,28 +1,17 @@
 import pool from "../config/db.js";
 
 export default class Project {
-  // Create Project
-
   static async createProject({ title, description, owner_id }) {
     const query = `INSERT INTO projects (title, description, owner_id) VALUES ($1, $2, $3) RETURNING *;`;
     const { rows } = await pool.query(query, [title, description, owner_id]);
     return rows[0];
   }
 
-  // Update Project
-
-  static async updateProject({ title, description, owner_id, id }) {
-    const query = `UPDATE projects SET title = $1, description = $2, owner_id = $3 WHERE id = $4 RETURNING *;`;
-    const { rows } = await pool.query(query, [
-      title,
-      description,
-      owner_id,
-      id,
-    ]);
+  static async updateProject({ title, description, updated_at, id }) {
+    const query = `UPDATE projects SET title = $1, description = $2, updated_at = $3 WHERE id = $4 RETURNING *;`;
+    const { rows } = await pool.query(query, [title, description, updated_at, id]);
     return rows[0];
   }
-
-  // Delete Project
 
   static async removeProject({ id }) {
     const query = `DELETE FROM projects WHERE id = $1 RETURNING *;`;
@@ -30,19 +19,15 @@ export default class Project {
     return rows[0];
   }
 
-  // Search Project For Owner
-
-  static async searchProject({ user_id }) {
-    const query = `SELECT * FROM projects WHERE owner_id = $1;`;
-    const { rows } = await pool.query(query, [user_id]);
-    return rows[0];
-  }
-
-  // Search Project with Project Id
-
-  static async searchProjectId({ project_id }) {
+  static async searchProjectById({ project_id }) {
     const query = `SELECT * FROM projects WHERE id = $1;`;
     const { rows } = await pool.query(query, [project_id]);
     return rows[0];
+  }
+
+  static async searchAllProjectsForUser({ user_id }) {
+    const query = `SELECT * FROM project_members WHERE user_id = $1;`;
+    const { rows } = await pool.query(query, [user_id]);
+    return rows;
   }
 }
