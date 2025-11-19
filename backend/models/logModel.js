a3c736cb5deb1552bdc7f8baa4d49e8f90c23d78
adisde <adityaa.sde@gmail.com>
2025-11-19 23:54:13 +0530

    update logs model (optimized)

--- backend/models/logModel.js
+++ backend/models/logModel.js
@@ -1,8 +1,6 @@
 import pool from "../config/db.js";
 
-
 export default class Log {
-    // Create Project Log
 
     static async createProjectLog({ user_id, project_id, log_message }) {
         const query = 'INSERT INTO project_logs (user_id, project_id, log_message) VALUES ($1, $2, $3) RETURNING *;';
@@ -10,15 +8,11 @@ export default class Log {
         return rows[0];
     }
 
-    // Get Project Logs
-
-    static async getProjectLogs({ project_id }) {
-        const query = "SELECT * FROM project_logs WHERE project_id = $1;";
-        const { rows } = await pool.query(query, [project_id]);
+    static async getProjectLogs({ project_id, limit = 20, offset = 0 }) {
+        const query = "SELECT * FROM project_logs WHERE project_id = $1 ORDER BY created_at DESC LIMIT $2 OFFSET $3;";
+        const { rows } = await pool.query(query, [project_id, limit, offset]);
         return rows;
     }
-
-    // Create User Log
 
     static async createUserLog({ user_id, log_message }) {
         const query = "INSERT INTO user_logs (user_id, log_message) VALUES ($1, $2) RETURNING *;";
@@ -26,11 +20,9 @@ export default class Log {
         return rows[0];
     }
 
-    // Get User Logs
-
-    static async getUserLogs({ user_id }) {
-        const query = "SELECT * FROM user_logs WHERE user_id = $1;";
-        const { rows } = await pool.query(query, [user_id]);
-        return rows[0];
+    static async getUserLogs({ user_id, limit = 20, offset = 0 }) {
+        const query = "SELECT * FROM user_logs WHERE user_id = $1 ORDER BY created_at DESC LIMIT $2 OFFSET $3;";
+        const { rows } = await pool.query(query, [user_id, limit, offset]);
+        return rows;
     }
 }
