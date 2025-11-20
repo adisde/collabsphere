4e7d3f420f38ca30050725e6f13fb80841f5a2d3
adisde <adityaa.sde@gmail.com>
2025-11-20 23:55:18 +0530

    update user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,10 +1,8 @@
 import pool from "../config/db.js";
 
 export default class User {
-  // Creating User
-
   static async createUser({ username, email, password, isverified = false }) {
-    const query = `INSERT INTO users (username, email, password, isverified) VALUES ($1, $2, $3, $4) RETURNING *;`;
+    const query = `INSERT INTO users (username, email, password, isverified) VALUES ($1, $2, $3, $4) RETURNING id, username, email;`;
     const { rows } = await pool.query(query, [
       username,
       email,
@@ -14,51 +12,45 @@ export default class User {
     return rows[0];
   }
 
-  // Updating Password
-
-  static async updatePassword({ email, password }) {
-    const query = `UPDATE users SET password = $2 WHERE email = $1 RETURNING *;`;
-    const { rows } = await pool.query(query, [email, password]);
+  static async updatePassword({ user_id, password }) {
+    const query = `UPDATE users SET password = $2, updated_at = NOW() WHERE id = $1 RETURNING id, username, email;`;
+    const { rows } = await pool.query(query, [user_id, password]);
     return rows[0];
   }
 
-  // Search Email
-
-  static async searchEmail({ email }) {
-    const query = `SELECT * FROM users WHERE email = $1`;
+  static async getUserForLogin({ email }) {
+    const query = `SELECT id, username, email, password, isverified FROM users WHERE email = $1 LIMIT 1;`;
     const { rows } = await pool.query(query, [email]);
     return rows[0];
   }
 
-  // Search Username
+  static async searchEmail({ email }) {
+    const query = `SELECT id, username, isverified, email FROM users WHERE email = $1 LIMIT 1;`;
+    const { rows } = await pool.query(query, [email]);
+    return rows[0];
+  }
 
   static async searchUsername({ username }) {
-    const query = `SELECT * FROM users WHERE username = $1`;
+    const query = `SELECT id, username, isverified, email FROM users WHERE username = $1 LIMIT 1;`;
     const { rows } = await pool.query(query, [username]);
     return rows[0];
   }
 
-  // Update Verification
-
-  static async updateVerification({ email, isverified = true }) {
-    const query = `UPDATE users SET isverified = $2 WHERE email = $1 RETURNING *`;
-    const { rows } = await pool.query(query, [email, isverified]);
+  static async updateVerification({ user_id }) {
+    const query = `UPDATE users SET isverified = TRUE, updated_at = NOW() WHERE id = $1 RETURNING id, username, email;`;
+    const { rows } = await pool.query(query, [user_id]);
     return rows[0];
   }
 
-  // Delete User
-
-  static async removeUser({ email }) {
-    const query = `DELETE FROM users WHERE email = $1 RETURNING *`;
-    const { rows } = await pool.query(query, [email]);
+  static async removeUser({ user_id }) {
+    const query = `DELETE FROM users WHERE id = $1 RETURNING id;`;
+    const { rows } = await pool.query(query, [user_id]);
     return rows[0];
   }
 
-  // Update user Details
-
-  static async updateUser({ username, email }) {
-    const query = `UPDATE users SET username = $1 WHERE email = $2 RETURNING *;`;
-    const { rows } = await pool.query(query, [username, email]);
+  static async updateUser({ username, user_id }) {
+    const query = `UPDATE users SET username = $1, updated_at = NOW() WHERE id = $2 RETURNING id, username, email;`;
+    const { rows } = await pool.query(query, [username, user_id]);
     return rows[0];
   }
 }
