7dbf22da81b6c65205974d61e858b0770a2b98ac
adisde <adityaa.sde@gmail.com>
2025-11-22 00:18:58 +0530

    update chat model

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -1,28 +1,38 @@
 import pool from "../config/db.js";
 
 export default class Chat {
-    // Create Chat 
+  static async createChat({ project_id, last_message }) {
+    const query =
+      "INSERT INTO chats (project_id, last_message, last_updated_at) VALUES ($1, $2, NOW()) RETURNING *;";
+    const { rows } = await pool.query(query, [project_id, last_message]);
+    return rows[0];
+  }
 
-    static async createChat({ project_id, last_message }) {
-        const query = "INSERT INTO chats (project_id, last_message) VALUES ($1, $2) RETURNING *;";
-        const { rows } = await pool.query(query, [project_id, last_message]);
-        return rows[0];
-    }
+  static async updateChat({ chat_id, last_message }) {
+    const query =
+      "UPDATE chats SET last_message = $2, last_updated_at = NOW() WHERE id = $1 RETURNING *;";
+    const { rows } = await pool.query(query, [
+      chat_id,
+      last_message,
+    ]);
+    return rows[0];
+  }
 
-    // Update Chat
+  static async getChat({ chat_id }) {
+    const query = "SELECT * FROM chats WHERE id = $1;";
+    const { rows } = await pool.query(query, [chat_id]);
+    return rows[0];
+  }
 
-    static async updateChat({ project_id, last_message, last_updated_at }) {
-        const query = "UPDATE chats SET last_message = $2, last_updated_at = $3 WHERE project_id = $1 RETURNING *;";
-        const { rows } = await pool.query(query, [project_id, last_message, last_updated_at]);
-        return rows[0];
-    }
+  static async getChatForProject({ project_id }) {
+    const query = "SELECT * FROM chats WHERE project_id = $1;";
+    const { rows } = await pool.query(query, [project_id]);
+    return rows[0];
+  }
 
-    // Get Chat
-
-    static async getChat({project_id}) {
-        const query = "SELECT * FROM chats WHERE project_id = $1;";
-        const {rows} = await pool.query(query, [project_id]);
-        return rows;
-    }
-
+  static async deleteChat({ id }) {
+    const query = "DELETE FROM chats WHERE id = $1 RETURNING *;";
+    const { rows } = await pool.query(query, [id]);
+    return rows[0];
+  }
 }
