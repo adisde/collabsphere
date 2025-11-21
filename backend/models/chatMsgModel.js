b75ff7d11181da71ee1366021b0330cb7d3acfba
adisde <adityaa.sde@gmail.com>
2025-11-22 00:10:11 +0530

    update chat message model

--- backend/models/chatMsgModel.js
+++ backend/models/chatMsgModel.js
@@ -1,19 +1,24 @@
 import pool from "../config/db.js";
 
 export default class ChatMsg {
-    // Create Chat Message
-
-    static async createChatMsg({ chat_id, sender_id, message, sent_at }) {
-        const query = "INSERT INTO chat_msg (chat_id, sender_id, message, sent_at) VALUES ($1, $2, $3, $4) RETURNING *;";
-        const { rows } = await pool.query(query, [chat_id, sender_id, message, sent_at]);
+    static async createChatMsg({ chat_id, sender_id, message }) {
+        const query = `
+            INSERT INTO chat_msg (chat_id, sender_id, message, sent_at) 
+            VALUES ($1, $2, $3, NOW()) 
+            RETURNING *;
+        `;
+        const { rows } = await pool.query(query, [chat_id, sender_id, message]);
         return rows[0];
     }
 
-    // Get Messages
-
     static async getMsgs({ chat_id }) {
-        const query = "SELECT * FROM chat_msg WHERE chat_id = $1;";
+        const query = `
+            SELECT * FROM chat_msg 
+            WHERE chat_id = $1 
+            ORDER BY sent_at ASC 
+            LIMIT 50;
+        `;
         const { rows } = await pool.query(query, [chat_id]);
-        return rows[0];
+        return rows;
     }
 }
