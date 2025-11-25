2835dbc044852d7b1af71b39ce5995b4e7926859
adisde <adityaa.sde@gmail.com>
2025-11-25 23:32:28 +0530

    update changes in user authorize

--- backend/middlewares/userAuthorize.js
+++ backend/middlewares/userAuthorize.js
@@ -4,28 +4,18 @@ export const userAuthorize = async (req, res, next) => {
   try {
     const header = req.headers.authorization;
 
-    if (!header || !header.startsWith("Bearer ")) {
-      return res.status(401).json({
-        messsage: "Missing headers for this request.",
-      });
-    }
+    if (!header || !header.startsWith("Bearer ")) return res.status(401).json({ ok: false, messsage: "Missing request headers." });
 
     const filterToken = header.split(" ")[1];
 
     const confirmToken = jwt.verify(filterToken, process.env.JWT_SECRET);
 
-    if (!confirmToken) {
-      return res.status(401).json({
-        messsage: "Invalid or expired token",
-      });
-    }
+    if (!confirmToken) return res.status(401).json({ ok: false, messsage: "Invalid token" });
 
-    req.user = { user_id: confirmToken.id };
+    req.user_id = confirmToken.id;
 
     next();
   } catch (err) {
-    return res.status(500).json({
-      messsage: "Something went wrong",
-    });
+    return res.status(500).json({ ok: false, messsage: "Something went wrong." });
   }
 };
