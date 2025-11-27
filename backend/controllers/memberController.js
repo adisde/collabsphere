c29fb637fbc63f3949ef1bf1e85cebd648d22d1d
adisde <adityaa.sde@gmail.com>
2025-11-27 23:27:17 +0530

    update changes in member controller

--- backend/controllers/memberController.js
+++ backend/controllers/memberController.js
@@ -1,211 +1,94 @@
+import { inputValidator } from "../helpers/inputsValidator.js";
 import Log from "../models/logModel.js";
 import Member from "../models/memberModel.js";
-import Project from "../models/projectModel.js";
-
-// Creating project member
 
 export const createProjectMember = async (req, res) => {
   try {
-    const { user_id, project_id, role } = req.body;
-    const { owner_id } = req.query;
+    const { user_id, role } = req.body;
+    const owner_id = req.user_id;
+    const project_id = req.id;
 
-    if (!user_id || !project_id || !owner_id) {
-      return res.status(400).json({
-        message: "Invalid or missing ids.",
-      });
-    }
+    const result = inputValidator(["user_id", "role"], req.body);
+    if (!result.ok) return res.status(400).json({ ok: false, message: result.message });
 
-    if (!role) {
-      return res.status(200).json({
-        message: "Invalid or missing member role.",
-      });
-    }
+   const adminMember = await Member.searchMember({user_id: owner_id, project_id});
+   if (!adminMember || adminMember.role !== "admin") return res.status(403).json({ok: false, message: "Unauthorized."});
 
-    const isExistMember = await Member.searchMember({
-      user_id: user_id,
-      project_id: project_id,
-    });
+    const isExistMember = await Member.searchMember({ user_id, project_id });
+    if (isExistMember) return res.status(200).json({ ok: true, message: "User exists in this project." });
 
-    if (isExistMember) {
-      return res.status(400).json({
-        message: "You're already exist in this project.",
-      });
-    }
+    if (role.trim() !== "member") return res.status(400).json({ ok: false, message: "Can't add this user with this role" });
 
-    const createMemberProject = await Member.createMember({
-      project_id: project_id,
-      user_id: user_id,
-      role: role,
-    });
+    const createMember = await Member.createMember({ project_id, user_id, role: role.trim() });
+    if (!createMember) return res.status(400).json({ ok: false, message: "Unable to add this user." });
 
-    if (!createMemberProject) {
-      return res.status(400).json({
-        message: "Failed to add this member for this project.",
-      });
-    }
+    await Log.createProjectLog({user_id: owner_id, project_id, log_message: `${owner_id} added ${user_id}.`})
 
-    await Log.createProjectLog({
-      user_id: owner_id,
-      project_id: project_id,
-      log_message: `${owner_id} added user ${user_id}`,
-    });
-
-    return res.status(200).json({
-      message: "Member has been added.",
-    });
+    return res.status(200).json({ ok: true, message: "Member added." });
   } catch (err) {
-    console.error("Creating member error : ", err.message);
-    return res.status(500).json({
-      message: "Something went wrong.",
-      sysMessage: err.message,
-    });
+    console.error("Create member error:", err.message);
+    return res.status(500).json({ ok: false, message: "Something went wrong." });
   }
 };
-
-// Updating project member
 
 export const updateProjectMember = async (req, res) => {
   try {
-    const { owner_id } = req.query;
-    const { role, user_id, project_id } = req.body;
-    console.log(req.body);
+    const owner_id = req.user_id;
+    const project_id = req.id;
+    const { role, user_id } = req.body;
 
-    if (!project_id || !user_id) {
-      return res.status(400).json({
-        message: "Invaild or missing ids.",
-      });
-    }
+    const result = inputValidator(["role", "user_id"], req.body);
+    if (!result.ok) return res.status(400).json({ ok: false, message: result.message });
 
-    if (!role) {
-      return res.status(400).json({
-        message: "Missing member role.",
-      });
-    }
+   const adminMember = await Member.searchMember({user_id: owner_id, project_id});
+   if (!adminMember || adminMember.role.trim() !== "admin") return res.status(403).json({ok: false, message: "Unauthorized."});
 
-    const isExistProject = await Project.searchProjectId({
-      project_id: project_id,
-    });
+    const isExistMember = await Member.searchMember({ user_id, project_id });
+    if (!isExistMember) return res.status(400).json({ ok: false, message: "Unknown user." });
 
-    if (!isExistProject) {
-      return res.status(400).json({
-        message: "Project does not exist.",
-      });
-    }
+    if (owner_id === user_id) return res.status(400).json({ ok: false, message: "You can't update yourself." });
 
-    const updateMemberPr = await Member.updateMember({
-      role: role,
-      user_id: user_id,
-      project_id: project_id,
-    });
+    if (role.trim() !== "member" && role.trim() !== "admin") return res.status(400).json({ok: false, message: "Invalid role."});
 
-    if (!updateMemberPr) {
-      return res.status(400).json({
-        message: "Failed to update the member role.",
-      });
-    }
+    const updateMember = await Member.updateMember({ role, user_id, project_id });
+    if (!updateMember) return res.status(400).json({ ok: false, message: "Unable to update member." });
+    
+    await Log.createProjectLog({user_id: owner_id, project_id, log_message: `${owner_id} updated ${user_id} to ${role}`})
 
-    await Log.createProjectLog({
-      user_id: owner_id,
-      project_id: project_id,
-      log_message: `${owner_id} updated the role as ${role} of user ${user_id}`,
-    });
-
-    return res.status(200).json({
-      message: "Member has been updated.",
-    });
+    return res.status(200).json({ ok: true, message: "Member updated." });
   } catch (err) {
-    console.error("Updating member error : ", err.message);
-    return res.status(500).json({
-      message: "Something went wrong.",
-      sysMessage: err.message,
-    });
+    console.error("Update member error:", err.message);
+    return res.status(500).json({ ok: false, message: "Something went wrong." });
   }
 };
-
-// Get all members for project
-
-export const getAllMembers = async (req, res) => {
-  try {
-    const { project_id } = req.query;
-
-    if (!project_id) {
-      return res.status(400).json({
-        message: "Invalid or missing project id",
-      });
-    }
-
-    const searchMembers = await Member.searchMembers({
-      project_id: project_id,
-    });
-
-    if (!searchMembers || searchMembers.length === 0) {
-      return res.status(400).json({
-        message: "Members does not exist for this project.",
-      });
-    }
-
-    return res.status(200).json({
-      message: "Search successful.",
-      res: searchMembers,
-    });
-  } catch (err) {
-    console.error("Getting members error : ", err.message);
-    return res.status(500).json({
-      message: "Something went wrong.",
-      sysMessage: err.message,
-    });
-  }
-};
-
-// Delete project member
 
 export const deleteProjectMember = async (req, res) => {
   try {
-    const { owner_id } = req.query;
-    const { project_id, user_id } = req.body;
-    
-    if (!project_id || !user_id || !owner_id) {
-      return res.status(400).json({
-        message: "Invalid or missing ids.",
-      });
-    }
+    const owner_id = req.user_id;
+    const project_id = req.id;
+    const { user_id } = req.query;
 
-    const isExistProjectForUser = await Member.searchMember({
-      user_id: user_id,
-      project_id: project_id,
-    });
+    const result = inputValidator(["user_id"], req.query);
+    if (!result.ok) return res.status(400).json({ ok: false, message: result.message });
 
-    if (!isExistProjectForUser) {
-      return res.status(400).json({
-        message: "Current member does not exist in this project.",
-      });
-    }
+    const adminMember = await Member.searchMember({user_id: owner_id, project_id});
+   if (!adminMember || adminMember.role.trim() !== "admin") return res.status(403).json({ok: false, message: "Unauthorized."});
 
-    const removeProjectMem = await Member.removeMember({
-      user_id: user_id,
-      project_id: project_id,
-    });
+    const isExistMember = await Member.searchMember({ user_id, project_id });
+    if (!isExistMember) return res.status(400).json({ ok: false, message: "Unknown user." });
 
-    if (!removeProjectMem) {
-      return res.status(400).json({
-        message: "Failed to remove member.",
-      });
-    }
+    if (isExistMember.role !== "member") return res.status(400).json({ok: false, message: "Can't delete this user."});
 
-    await Log.createProjectLog({
-      user_id: owner_id,
-      project_id: project_id,
-      log_message: `${owner_id} removed user ${user_id}`,
-    });
+    if (owner_id === user_id) return res.status(400).json({ ok: false, message: "You can't delete yourself." });
 
-    return res.status(200).json({
-      message: "Member has been removed.",
-    });
+    const deleteMember = await Member.removeMember({ user_id, project_id });
+    if (!deleteMember) return res.status(400).json({ ok: false, message: "Unable to remove the user." });
+
+    await Log.createProjectLog({user_id: owner_id, project_id, log_message: `${owner_id} removed ${user_id}.`});
+
+    return res.status(200).json({ ok: true, message: "Member removed." });
   } catch (err) {
-    console.error("Deleting member error : ", err.message);
-    return res.status(500).json({
-      message: "Something went wrong.",
-      sysMessage: err.message,
-    });
+    console.error("Delete member error:", err.message);
+    return res.status(500).json({ ok: false, message: "Something went wrong." });
   }
 };
