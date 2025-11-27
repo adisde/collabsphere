f8b0feae5a11722235f347bd7ea8718803378a58
adisde <adityaa.sde@gmail.com>
2025-11-27 23:35:19 +0530

    update changes and added pagination

--- backend/models/chatMsgModel.js
+++ backend/models/chatMsgModel.js
@@ -1,24 +1,39 @@
 import pool from "../config/db.js";
 
 export default class ChatMsg {
-    static async createChatMsg({ chat_id, sender_id, message }) {
-        const query = `
+  static async createChatMsg({ chat_id, sender_id, message }) {
+    const query = `
             INSERT INTO chat_msg (chat_id, sender_id, message, sent_at) 
             VALUES ($1, $2, $3, NOW()) 
             RETURNING *;
         `;
-        const { rows } = await pool.query(query, [chat_id, sender_id, message]);
-        return rows[0];
-    }
+    const { rows } = await pool.query(query, [chat_id, sender_id, message]);
+    return rows[0];
+  }
 
-    static async getMsgs({ chat_id }) {
-        const query = `
+  static async getMsgs({ chat_id, limit, offset }) {
+    const query = `
             SELECT * FROM chat_msg 
             WHERE chat_id = $1 
             ORDER BY sent_at ASC 
-            LIMIT 50;
+            LIMIT $2 OFFSET $3;   
         `;
-        const { rows } = await pool.query(query, [chat_id]);
-        return rows;
-    }
+    const { rows } = await pool.query(query, [chat_id, limit, offset]);
+    return rows;
+  }
+
+  static async getAllMsgsCount({ chat_id }) {
+    const query = `
+            SELECT COUNT(*) FROM chat_msg 
+            WHERE chat_id = $1;
+        `;
+    const { rows } = await pool.query(query, [chat_id]);
+    return parseInt(rows[0].count);
+  }
+
+  static async removeAllMessageForProject({ chat_id }) {
+    const query = `DELETE FROM chat_msg WHERE chat_id = $1 RETURNING *;`;
+    const { rows } = await pool.query(query, [chat_id]);
+    return rows.length;
+  }
 }
