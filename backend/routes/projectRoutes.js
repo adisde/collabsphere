f512c85f528693676c00aa46154a004ca2f26496
adisde <adityaa.sde@gmail.com>
2025-11-30 21:03:29 +0530

    include middlewares in routes

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,13 +1,16 @@
 import express from "express";
-import { createNewProject, deleteExistProject, getProject, updateExistProject } from "../controllers/projectController.js";
+import { createNewProject, getAllProjectsForUser, getSingleProjectForUser, removeExistingProject, updateExistingProject } from "../controllers/projectController.js";
+import { userAuthorize } from "../middlewares/userAuthorize.js";
+import { adminAuthorize } from "../middlewares/adminAuthorize.js";
+import { projectAuthorize } from "../middlewares/projectAuthorize.js";
 
 const router = express.Router();
 
-// Project Routes
+router.post("/create", userAuthorize, createNewProject);
+router.put("/update/:project_id", userAuthorize, adminAuthorize, updateExistingProject);
+router.delete("/delete/:project_id", userAuthorize, adminAuthorize, removeExistingProject);
 
-router.post("/", createNewProject);
-router.get("/get", getProject);
-router.put("/update", updateExistProject);
-router.delete("/delete", deleteExistProject);
+router.get("/get-project/:project_id", userAuthorize, projectAuthorize, getSingleProjectForUser);
+router.get("/get-all", userAuthorize, getAllProjectsForUser);
 
 export default router;
