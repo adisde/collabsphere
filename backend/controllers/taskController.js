e6bc1f6729c28d602ee96a5178c8e570ad794808
adisde <adityaa.sde@gmail.com>
2025-12-02 23:30:52 +0530

    include update status end point func and changes

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,283 +1,123 @@
+import { inputValidator } from "../helpers/inputsValidator.js";
 import Log from "../models/logModel.js";
 import Member from "../models/memberModel.js";
-import Project from "../models/projectModel.js";
 import Task from "../models/taskModel.js";
-
-// Creating project task
 
 export const createProjectTask = async (req, res) => {
   try {
-    const { project_id, title, description, assigned_to, due_date, status } =
-      req.body;
+    const { title, description, assigned_to, due_date, status } = req.body;
 
-    const { owner_id } = req.query;
+    const owner_id = req.user_id;
+    const project_id = req.id;
 
-    if (!project_id || !owner_id) {
-      return res.status(400).json({
-        message: "Invalid or missing ids.",
-      });
-    }
+    const result = inputValidator(["title", "assigned_to", "due_date", "status"], req.body);
+    if (!result.ok) return res.status(400).json({ ok: false, message: result.message });
 
-    if (!title || !assigned_to || !due_date || !status) {
-      return res.status(400).json({
-        message: "Missing task information.",
-      });
-    }
+    const isExistUserInProject = await Member.searchMember({ user_id: assigned_to, project_id });
+    if (!isExistUserInProject) return res.status(400).json({ ok: false, message: "User does not exist." });
 
-    const isExistProject = await Project.searchProjectId({ project_id });
+    if (title.trim().length <= 3) return res.status(400).json({ ok: false, message: "Task title length must be longer than 3 characters." });
 
-    if (!isExistProject) {
-      return res.status(400).json({
-        message: "Project does not exist.",
-      });
-    }
+    const validStatus = ["todo", "inprogress", "completed"];
+    if (!validStatus.includes(status.trim())) return res.status(400).json({ ok: false, message: "Invalid status." });
 
-    const isExistOwner = await Member.searchMember({
-      user_id: owner_id,
-      project_id,
-    });
+    const createNewTask = await Task.createTask({ assigned_to, project_id, title, description, status, due_date });
+    if (!createNewTask) return res.status(400).json({ ok: false, message: "Unable to create task." });
 
-    if (!isExistOwner) {
-      return res.status(400).json({
-        message: "U're not a part of this project.",
-      });
-    }
+    await Log.createProjectLog({ user_id: owner_id, project_id, log_message: `${owner_id} assgined task to ${assigned_to}.` });
 
-    if (isExistOwner.role !== "admin") {
-      return res.status(400).json({
-        message: "U can't add tasks to this project.",
-      });
-    }
-
-    const isExistAssginee = await Member.searchMember({
-      user_id: assigned_to,
-      project_id,
-    });
-
-    if (!isExistAssginee) {
-      return res.status(400).json({
-        message: "This user not exist in the project.",
-      });
-    }
-
-    const createPrTask = await Task.createTask({
-      assigned_to,
-      project_id,
-      title,
-      description,
-      status,
-      due_date,
-    });
-
-    if (!createPrTask) {
-      return res.status(400).json({
-        message: "Failed to create task.",
-      });
-    }
-
-    await Log.createProjectLog({
-      user_id: owner_id,
-      project_id,
-      log_message: `${owner_id} assigned a task to ${assigned_to}`,
-    });
-
-    return res.status(200).json({
-      message: "Task has been created.",
-    });
+    return res.status(201).json({ ok: true, message: "Task created and assigned.", task: createNewTask});
   } catch (err) {
-    console.error("Creating task error : ", err.message);
-    return res.status(500).json({
-      message: "Something went wrong.",
-      sysMessage: err.message,
-    });
+    console.error("Create task error:", err.message);
+    return res.status(500).json({ ok: false, message: "Something went wrong." });
   }
 };
-
-// Update Existing Task
 
 export const updateProjectTask = async (req, res) => {
   try {
-    const { id } = req.query;
-    const {
-      title,
-      description,
-      due_date,
-      status,
-      assigned_to,
-      user_id,
-      project_id,
-    } = req.body;
+    const { title, description, assigned_to, due_date, status } = req.body;
 
-    if (!id || !user_id || !project_id) {
-      return res.status(400).json({
-        message: "Invalid or missing ids.",
-      });
-    }
+    const owner_id = req.user_id;
+    const project_id = req.id;
+    const id = req.query.task_id;
 
-    if (!title || !due_date || !status) {
-      return res.status(400).json({
-        message: "Missing task information.",
-      });
-    }
+    const result = inputValidator(["title", "assigned_to", "due_date", "status"], req.body);
+    if (!result.ok) return res.status(400).json({ ok: false, message: result.message });
 
-    const isExistProject = await Project.searchProjectId({ project_id });
+    if (title.trim().length <= 3) return res.status(400).json({ ok: false, message: "Task title length must be longer than 3 characters." });
 
-    if (!isExistProject) {
-      return res.status(400).json({
-        message: "Project does not exist.",
-      });
-    }
+    const validStatus = ["todo", "inprogress", "completed"];
+    if (!validStatus.includes(status.trim())) return res.status(400).json({ ok: false, message: "Invalid status." });
 
-    const isExistUserInProject = await Member.searchMember({
-      user_id,
-      project_id,
-    });
-
-    if (!isExistUserInProject) {
-      return res.status(400).json({
-        message: "This user does not exist for this project.",
-      });
-    }
+    const isExistUserInProject = await Member.searchMember({ user_id: assigned_to, project_id });
+    if (!isExistUserInProject) return res.status(400).json({ ok: false, message: "User does not exist" });
 
     const isExistTask = await Task.getSingleTask({ id });
+    if (!isExistTask) return res.status(404).json({ ok: false, message: "Task not found." });
 
-    if (!isExistTask) {
-      return res.status(400).json({
-        message: "Task does not exist.",
-      });
+    const updateExistTask = await Task.updateTask({ assigned_to, id, title, description, status, due_date });
+    if (!updateExistTask) return res.status(400).json({ ok: false, message: "Unable to update task." });
+
+    if (updateExistTask.assigned_to !== assigned_to) {
+      await Log.createProjectLog({ user_id: owner_id, project_id, log_message: `${owner_id} assigned task to ${assigned_to}.` });
     }
 
-    if (project_id !== isExistProject.id) {
-      return res.status(400).json({
-        message: "Task does not exist in project.",
-      });
-    }
-
-    if (
-      isExistUserInProject.role !== "admin" &&
-      isExistTask.assigned_to !== user_id
-    ) {
-      return res.status(403).json({
-        message: "U can't update this task.",
-      });
-    }
-
-    if (
-      isExistUserInProject.role !== "admin" &&
-      assigned_to !== isExistTask.assigned_to
-    ) {
-      return res.status(403).json({
-        message: "Only admins can reassign tasks.",
-      });
-    }
-
-    const updatePrTask = await Task.updateTask({
-      assigned_to,
-      id,
-      title,
-      description,
-      status,
-      due_date,
-    });
-
-    if (!updatePrTask) {
-      return res.status(400).json({
-        message: "Failed to update the task.",
-      });
-    }
-
-    await Log.createProjectLog({
-      user_id,
-      project_id,
-      log_message: `${user_id} updated the task.`,
-    });
-
-    return res.status(200).json({
-      message: "Task has been updated.",
-    });
+    await Log.createProjectLog({ user_id: owner_id, project_id, log_message: `${owner_id} updated task details.` });
+    return res.status(200).json({ ok: true, message: "Task updated.", task: updateExistTask});
   } catch (err) {
-    console.error("Updating task error : ", err.message);
-    return res.status(500).json({
-      message: "Something went wrong.",
-      sysMessage: err.message,
-    });
+    console.error("Update task error:", err.message);
+    return res.status(500).json({ ok: false, message: "Something went wrong." });
   }
 };
-
-// Deleting the task
 
 export const deleteProjectTask = async (req, res) => {
   try {
-    const { id } = req.query;
-    const { user_id, project_id } = req.body;
+    const id = req.query.id;
+    const owner_id = req.user_id;
+    const project_id = req.id;
 
-    if (!id || !user_id || !project_id) {
-      return res.status(400).json({
-        message: "Invalid or missing ids.",
-      });
-    }
+    if (!id) return res.status(400).json({ ok: false, message: "Task id is required." });
 
-    const isExistTaskForProject = await Task.getSingleTask({ id });
+    const isExistTask = await Task.getSingleTask({ id });
+    if (!isExistTask) return req.status(404).json({ ok: false, message: "Task not found." });
 
-    if (!isExistTaskForProject) {
-      return res.status(400).json({
-        message: "Task does not exist for this project.",
-      });
-    }
+    const deleteTask = await Task.removeTask({ id });
+    if (!deleteTask) return res.status(400).json({ ok: false, message: "Unable to create task." });
 
-    if (isExistTaskForProject.project_id !== project_id) {
-      return res.status(200).json({
-        message: "This task does not belong to this project.",
-      });
-    }
-
-    const isExistProject = await Project.searchProjectId({ project_id });
-
-    if (!isExistProject) {
-      return res.status(400).json({
-        message: "Project does not exist.",
-      });
-    }
-
-    const isExistOwner = await Member.searchMember({
-      user_id,
-      project_id,
-    });
-
-    if (!isExistOwner) {
-      return res.status(400).json({
-        message: "U're not a part of this project.",
-      });
-    }
-
-    if (isExistOwner.role !== "admin") {
-      return res.status(400).json({
-        message: "U can't delete this task.",
-      });
-    }
-
-    const deletePrTask = await Task.removeTask({ id });
-
-    if (!deletePrTask) {
-      return res.status(400).json({
-        message: "Failed to delete task.",
-      });
-    }
-
-    await Log.createProjectLog({
-      user_id,
-      project_id,
-      log_message: `${user_id} deleted a task ${id}.`,
-    });
-
-    return res.status(200).json({
-      message: "Task has been deleted for this project.",
-    });
+    await Log.createProjectLog({ user_id: owner_id, project_id, log_message: `${owner_id} deleted task${id}.` });
+    return res.status(200).json({ ok: true, message: "Task deleted." });
   } catch (err) {
-    console.error("Deleting task error : ", err.message);
-    return res.status(500).json({
-      message: "Something went wrong.",
-      sysMessage: err.message,
-    });
+    console.error("Delete task error:", err.message);
+    return res.status(500).json({ ok: false, message: "Something went wrong." });
   }
 };
+
+export const updateTaskStatus = async (req, res) => {
+  try {
+    const id = req.query.task_id;
+    const { status } = req.body;
+    const user_id = req.user_id;
+    const project_id = req.project_id;
+
+    const result = inputValidator(["status"], req.body);
+    if (!result.ok) return res.status(400).json({ ok: false, message: result.message });
+
+    const validStatus = ["todo", "inprogress", "completed"];
+    if (!validStatus.includes(status.trim())) return res.status(400).json({ ok: false, message: "Invalid status." });
+
+    if (!id) return res.status(400).json({ ok: false, message: "Task id is required." });
+
+    const isExistUserInForTask = await Task.getSingleTask({ id });
+    if (!isExistUserInForTask) return res.status(404).json({ ok: false, message: "Task not found." });
+    if (isExistUserInForTask.assigned_to !== user_id) return res.status(400).json({ ok: false, message: "Can't update someone's task." });
+
+    const updateTask = await Task.updateStatus({ id, status });
+    if (!updateTask) return res.status(400).json({ ok: false, message: "Unable to update task." });
+
+    await Log.createProjectLog({ user_id, project_id, log_message: `${user_id} update task status to ${status}.` });
+    return res.status(200).json({ ok: true, message: "Task status updated." });
+  } catch (err) {
+    console.error("Update task status error:", err.message);
+    return res.status(500).json({ ok: false, message: "Something went wrong." });
+  }
+}
