87f8f2c51bb3466e2a021f8157d19c2c8d4010a1
adisde <adityaa.sde@gmail.com>
2025-12-02 23:19:26 +0530

    include middleware on routes

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { createProjectTask, deleteProjectTask, updateProjectTask } from "../controllers/taskController.js";
+import { createProjectTask, deleteProjectTask, updateProjectTask, updateTaskStatus } from "../controllers/taskController.js";
+import { userAuthorize } from "../middlewares/userAuthorize.js";
+import { adminAuthorize } from "../middlewares/adminAuthorize.js";
+import { projectAuthorize } from "../middlewares/projectAuthorize.js";
 
 const router = express.Router();
 
-// Task Routes
+router.post("/create/:project_id", userAuthorize, adminAuthorize, createProjectTask);
+router.put("/update/:project_id", userAuthorize, adminAuthorize, updateProjectTask);
+router.delete("/delete/:project_id", userAuthorize, adminAuthorize, deleteProjectTask);
 
-router.post("/", createProjectTask);
-router.put("/update", updateProjectTask);
-router.delete("/delete", deleteProjectTask);
+router.put("/update-status/:project_id", userAuthorize, projectAuthorize, updateTaskStatus);
 
 export default router;
