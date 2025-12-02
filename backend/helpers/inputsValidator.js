161b498838e873d21080e6599b36e156c764a10e
adisde <adityaa.sde@gmail.com>
2025-12-02 23:24:23 +0530

    update changes with type checking (strings only)

--- backend/helpers/inputsValidator.js
+++ backend/helpers/inputsValidator.js
@@ -1,11 +1,15 @@
 export const inputValidator = (fields = [], body = {}) => {
   for (let f of fields) {
-    if (!body[f] || body[f].trim() === "") {
-      return {
-        ok: false,
-        message: `${f} is required.`,
-      };
+    const value = body[f];
+
+    if (value === undefined || value === null) {
+      return { ok: false, message: `${f} is required.` };
+    }
+
+    if (String(value).trim() === "") {
+      return { ok: false, message: `${f} cannot be empty.` };
     }
   }
+
   return { ok: true };
 };
