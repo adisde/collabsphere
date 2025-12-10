7698934355de7a45053a6aa113a233dd37b56cfc
adisde <adityaa.sde@gmail.com>
2025-12-10 21:48:05 +0530

    update changes in login hook

--- collabsphere/src/hooks/auth/login.ts
+++ collabsphere/src/hooks/auth/login.ts
@@ -2,8 +2,8 @@ const baseUrl = import.meta.env.VITE_SERVER_URL;
 
 export const login = async (email: string, password: string) => {
     try {
-        if (!email || email.trim() === "") return { ok: false, message: "Email is required" };
-        if (!password || password.trim() === "") return { ok: false, message: "Password is required" };
+        if (!email || email.trim() === "") return { ok: false, message: "Email is required." };
+        if (!password || password.trim() === "") return { ok: false, message: "Password is required." };
         if (password.length <= 8) return { ok: false, message: "Password must be longer than 8 characters." };
 
         const loginPayload = {
@@ -22,8 +22,8 @@ export const login = async (email: string, password: string) => {
         const res = await req.json();
 
         if (!res.ok) return { ok: false, message: res.message };
-
         return { ok: true, message: res.message };
+
     } catch (err: any) {
         return { ok: false, message: err.message };
     }
